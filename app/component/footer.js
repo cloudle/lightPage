319256a627b619cce17bef3882559a9f93f59e8e
Cloud Le <lehaoson@gmail.com>
2016-05-12 11:13:42 +0700

    Add animation on page transition!

--- app/component/footer.js
+++ app/component/footer.js
@@ -9,8 +9,10 @@ export default [function () {
 						<div class="heading">RIVER CITY</div>
 						<div>River City mang ý nghĩa “Thành phố ven sông”, với tham vọng sẽ tạo dựng lên một thành phố thu nhỏ với nhiều tiện ích bên cạnh Sông Sài Gòn. River City là dự án khu căn hộ có quy mô lớn nhất Nam Sài Gòn, được phát triển bởi An Gia - Creed Group (Nhật Bản) - Phát Đạt với tổng vốn đầu tư lên đến 500 triệu USD.</div>
 					</div>
-					<div class="col"></div>
-					<div class="col"></div>
+					<div class="col">
+						<div class="heading">LIÊN HỆ</div>
+						<div>Liên hệ tham quan dự án và chọn những vị trí đẹp nhất ngay từ bây giờ, Chúng tôi sẽ hỗ trợ nhiệt tình cho Quý Khách 24/7.</div>
+						</div>
 				</div>
 		
 				<!--<div class="footer-links">-->
