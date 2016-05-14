7e5a770c97b944dc39d225c0cbf5384d1299d11e
Cloud Le <lehaoson@gmail.com>
2016-05-14 16:15:36 +0700

    Slider's handler (NEXT/PREVIOUS) - Swipe support!

--- app/component/slider.js
+++ app/component/slider.js
@@ -4,12 +4,18 @@ export default ['$interval', '$timeout', function ($interval, $timeout) {
 		replace: true,
 		scope: { items: '=' },
 		transclude: true,
-		template: `<div class="light-slider {{wrapperClass}}">	
+		template: `<div class="light-slider {{wrapperClass}}"
+			hm-swipeLeft="swipeLeft($event)" hm-swipeRight="swipeRight($event)">
 			<div id="currentSlide" class="active-slide" ng-style="{'background-image': 'url('+activeSlide.image+')'}"></div>
 			<div id="previousSlide" class="active-slide previous" ng-style="{'background-image': 'url('+previousSlide.image+')'}"></div>
-	
+
+			<div class="slide-navigation">
+				<div class="navigate-next"></div>
+				<div class="navigate-back"></div>
+			</div>
+
 			<div class="slide-positions">
-				<div class="position-item" ng-repeat="item in items"></div>
+				<div class="position-item" ng-class="{active: item.isActive}" ng-repeat="item in items" ng-click="navigate(item)"></div>
 			</div>
 			<ng-transclude></ng-transclude>
 		</div>`,
@@ -28,20 +34,37 @@ export default ['$interval', '$timeout', function ($interval, $timeout) {
 
 			let nextSlide = function (nextIndex) {
 				scope.previousSlide = scope.items[scope.activeIndex];
+				if (scope.previousSlide) scope.previousSlide.isActive = false;
 
-				scope.activeIndex = nextIndex || scope.activeIndex + 1;
-				if (scope.activeIndex >= scope.items.length || scope.activeIndex < 0) {
+				scope.activeIndex = nextIndex != undefined ? nextIndex : scope.activeIndex + 1;
+				if (scope.activeIndex < 0) {
+					scope.activeIndex = scope.items.length - 1;
+				} else if (scope.activeIndex >= scope.items.length) {
 					scope.activeIndex = 0;
 				}
 
 				scope.activeSlide = scope.items[scope.activeIndex];
+				if (scope.activeSlide) scope.activeSlide.isActive = true;
 
 				//Play transition animation!
 				// TweenLite.fromTo($previousSlide, transitionTime, {ease: easeEffect, x: '0%'}, {ease: easeEffect, x: '100%'});
 				// TweenLite.fromTo($activeSlide, transitionTime, {ease: easeEffect, x: '-100%'}, {ease: easeEffect, x: '0%'});
 				TweenLite.to($activeSlide, 0, {ease: easeEffect, opacity: '1'});
-				TweenLite.fromTo($previousSlide, transitionTime, {ease: easeEffect, opacity: '1'}, {ease: easeEffect, opacity: '0'})
+				TweenLite.fromTo($previousSlide, transitionTime, {ease: easeEffect, opacity: '1'}, {ease: easeEffect, opacity: '0'});
+
+				//Reset interval;
+				if (global.sliderInterval) $interval.cancel(global.sliderInterval);
+				global.sliderInterval = $interval(() => nextSlide(), 10000);
 			};
+
+			scope.navigate = (instance) => {
+				if (instance != scope.activeSlide) {
+					nextSlide(scope.items.indexOf(instance));
+				}
+			};
+
+			scope.swipeLeft = (e) => nextSlide(scope.activeIndex + 1);
+			scope.swipeRight = (e) => nextSlide(scope.activeIndex - 1);
 
 			global.sliderInterval = $interval(() => nextSlide(), 10000);
 		}
