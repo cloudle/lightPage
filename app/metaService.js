e9ce447673349ea3e3dd5051b04b3fb9856b7ccb
Cloud Le <lehaoson@gmail.com>
2016-05-20 18:25:42 +0700

    Changes which support multiple sites.

--- app/metaService.js
+++ app/metaService.js
@@ -4,14 +4,13 @@ export default ['$rootScope', '$http', '$timeout', function ($rootScope, $http, 
 	this.promise = new Promise((configResolve, reject) => {
 		$http.get('/configs').success((data) => {
 			data.domain = data.domain || location.host;
-			console.log(data.domain);
 			let configs = data, { apiHost, domain } = configs;
 
 			new Promise((navigationResolve, reject) => {
 				$http.get(`${apiHost}/menu/get/json`, {
 					params: { domain }
 				}).success((data) => {
-					this.links = data.results; this.configs = configs;
+					this.links = data.results; this.configs = configs; console.log(this.links);
 					navigationResolve(this.links);
 					configResolve(this.configs);
 					$timeout(() => {
