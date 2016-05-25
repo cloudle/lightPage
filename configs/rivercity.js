95e073b115dbea2ec2769b8c1629a4b3c4e4c490
Cloud Le <lehaoson@gmail.com>
2016-05-25 11:51:53 +0700

    RiverCity Meta Update!

--- configs/rivercity.js
+++ configs/rivercity.js
@@ -1,16 +1,21 @@
 module.exports = {
-	"domain": "rivercity99.vn",
-	"siteName": "River City",
-	"siteFav": "images/favicon.ico",
-	"serverPort": 7020,
-	"production": true,
-	"apiHost": "http://103.56.157.66/realestate",
-	"googleAnalyticId": "UA-77901914-1",
-	"facebookAppId": "108597779162841",
-	"facebookPixelId": "578115232338331",
-	"antsSiteId": "578664336",
-	"antsRegisterGoalId": "578664668",
-	"translation": {
-		"hotline": "0932 047 313"
+	domain: "rivercity99.vn",
+	siteName: "River City",
+	siteFav: "images/favicon.ico",
+	serverPort: 7020,
+	production: true,
+	apiHost: "http://103.56.157.66/realestate",
+	googleAnalyticId: "UA-77901914-1",
+	facebookAppId: "108597779162841",
+	facebookPixelId: "578115232338331",
+	antsSiteId: "578664336",
+	antsRegisterGoalId: "578664668",
+	ogTitle: "RIVER CITY - TUYỆT TÁC BÊN SÔNG SÀI GÒN.",
+	ogImage: "http://farm8.staticflickr.com/7369/27023964906_63062c471b_b.jpg",
+	ogDescription: `River City là dự án khu căn hộ có quy mô lớn nhất Nam Sài Gòn với 3 mặt tiếp giáp sông, được phát triển bởi An Gia - Creed Group (Nhật Bản) - Phát Đạt với tổng vốn đầu tư lên đến 500 triệu USD. 
+		Với 4 tiện ích ưu việt: QUẢNG TRƯỜNG NƯỚC VÀ ÁNH SÁNG - BIỂN ĐẢO NHÂN TẠO DUBAI - HỒ BƠI THÁC NƯỚC 2 TẦNG - ĐƯỜNG HOA ĐI BỘ TRÊN KHÔNG
+    Gọi 0932 047 313 (24/7) để nhận  THÔNG TIN CHÍNH THỨC từ CHỦ ĐẦU TƯ`,
+	translation: {
+		hotline: "0932 047 313"
 	}
 };
