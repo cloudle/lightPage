108244a40a3090719dfb9e3e1009acca6acbb8e4
Cloud Le <lehaoson@gmail.com>
2016-05-27 19:17:04 +0700

    Publishing Golden River!

--- configs/goldenriver.js
+++ configs/goldenriver.js
@@ -1,7 +1,7 @@
 module.exports = {
-	siteName: "Golden River",
-	g: "images/logo-goldenriver.jpg",
 	domain: "goldenrivervietnam.vn",
+	siteName: "Golden River",
+	siteFav: "images/logo-goldenriver.jpg",
 	serverPort: 7021,
 	production: true,
 	apiHost: "http://103.56.157.66/realestate",
@@ -13,9 +13,9 @@ module.exports = {
 	antsSiteId: "574501431",
 	antsRegisterGoalId: "574522411",
 	ogTitle: "VINHOMES GOLDEN RIVER - ĐỈNH CAO CỦA CUỘC SỐNG THƯỢNG LƯU",
+	ogImage: "https://farm8.staticflickr.com/7163/26581800354_b82752ef56_o.jpg",
 	ogDescription: `Lần đầu tiên tại Việt Nam, bên dòng sông hiền hòa, giữa phố phường sôi động. Xuất hiện một quần thể đô thị sinh thái nằm trong lòng quận nhất. VINHOMES GOLDEN RIVER tọa lạc tại BA SON - quần thể đô thị sang trọng và hiện đại bật nhất Việt Nam. Mảnh đất kim cương giữa trung tâm thành phố. Trải nghiệm cuộc sống đẳng cấp, tận hưởng những tiện ích hoàn hảo.
 		Gọi 0902 623 673 (24/7) để nhận THÔNG TIN CHÍNH THỨC từ CHỦ ĐẦU TƯ`,
-	ogImage: "https://farm8.staticflickr.com/7163/26581800354_b82752ef56_o.jpg",
 	translation: {
 		hotline: "0902 623 673"
 	}
