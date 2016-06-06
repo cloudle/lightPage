630b06e810e638d33a23db9aa5bc5e431f43fa0e
Cloud Le <lehaoson@gmail.com>
2016-06-06 17:21:32 +0700

    Toyota PMH deploy (first time).

---
+++ configs/toyotaPmh.js
@@ -0,0 +1,29 @@
+module.exports = {
+  domain: "toyota-phumyhung.com.vn",
+  siteName: "toyotaPmh",
+  siteFav: "images/logo-urv.png",
+  serverPort: 7023,
+  production: true,
+  apiHost: "http://103.56.157.66/realestate",
+  googleAnalyticId: "UA-77901914-4",
+  facebookAppId: "",
+  facebookPixelId: "",
+  antsSiteId: "",
+  antsRegisterGoalId: "",
+  ogTitle: "United Realtors Vietnam - HỆ THỐNG PHÂN PHỐI BẤT ĐỘNG SẢN SỬ DỤNG CÔNG NGHỆ VƯỢT TRỘI",
+  ogImage: "https://c4.staticflickr.com/8/7425/27299346851_b454219c5a_o.jpg",
+  ogDescription: `Công ty UNITED REALTORS Việt Nam tự hào ứng dụng công nghệ siêu việt, đọc hiểu nhu cầu mua nhà của khách hàng quan tâm các dự án căn hộ Hot nhất khu Nam Sài Gòn.
+		Mỗi ngày hệ thống tiếp nhận hàng trăm lượt đăng ký quan tâm mua căn hộ.
+		Anh chị em tư vấn viên có nhiều năm kinh nghiệm và am hiểu thị trường Quận 7 hãy liên hệ ngay với chúng tôi để được cài đặt ứng dụng & nhận khách để bán hàng.`,
+  translation: {
+    hotline: "0932 047 383",
+    vietnamese: {
+      registerTitleTail: `
+			<span class="ultra strong" ng-bind="configs.translation.hotline"></span>
+			<span> hoặc gửi thông tin để</span> 
+			<span class="strong">THAM GIA</span>
+			<span>làm</span> 
+			<span class="strong">CỘNG TÁC VIÊN</span>`
+    }
+  }
+};
