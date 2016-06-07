3432b371ce4704be56725a6010d056f5ee823e59
nglocjk <nglocjk@live.com>
2016-06-07 15:49:16 +0700

    Anphaland deploy (first time)

---
+++ configs/anphaland.js
@@ -0,0 +1,18 @@
+module.exports = {
+	domain: "anphaland.com.vn",
+	siteName: "Anphanland",
+	siteFav: "images/logo-goldenriver.jpg",
+	serverPort: 7024,
+	production: true,
+	apiHost: "http://103.56.157.66/realestate",
+	scriptPatch: "./app/patches/anphaland.js",
+	cssPatch: "./app/style/patches/anphaland.scss",
+	googleAnalyticId: "UA-77901914-2",
+	facebookAppId: "100493957008881",
+	facebookPixelId: "483381658466682",
+	antsSiteId: "574501431",
+	antsRegisterGoalId: "574522411",
+	translation: {
+		"hotline": "0932 047 383"
+	}
+};
