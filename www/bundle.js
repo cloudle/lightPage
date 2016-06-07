56c4eb7358b875f91dd91ca609fa66834f350c22
nglocjk <nglocjk@live.com>
2016-06-07 18:28:44 +0700

    RiverCity deploy

--- www/bundle.js
+++ www/bundle.js
@@ -307,7 +307,7 @@ exports.default = ['$rootScope', '$http', 'metaService', function ($rootScope, $
 
 var fields = ['userName', 'userPhone', 'userEmail'];
 
-},{"../utils/helper":19}],9:[function(require,module,exports){
+},{"../utils/helper":18}],9:[function(require,module,exports){
 (function (global){
 'use strict';
 
@@ -540,7 +540,7 @@ var applicationController = exports.applicationController = function application
 applicationController.$inject = ['$rootScope', '$scope', '$state', '$timeout', '$interval', '$window', '$http', 'ngProgressFactory', 'metaService'];
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"../utils/helper":19}],10:[function(require,module,exports){
+},{"../utils/helper":18}],10:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -896,7 +896,7 @@ App.filter('unsafe', ['$sce', function ($sce) {
 angular.bootstrap(document, ['application']);
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"./component/footer":1,"./component/navigation":2,"./component/navigationLink":3,"./component/newsArea":4,"./component/popup":5,"./component/sidebar":6,"./component/slider":7,"./component/subscriptionForm":8,"./controller/applicationController":9,"./controller/mainController":10,"./controller/newsController":11,"./controller/pageController":12,"./controller/splashController":13,"./metaService":15,"./routerConfig":17,"./utils/filter":18,"./utils/helper":19}],15:[function(require,module,exports){
+},{"./component/footer":1,"./component/navigation":2,"./component/navigationLink":3,"./component/newsArea":4,"./component/popup":5,"./component/sidebar":6,"./component/slider":7,"./component/subscriptionForm":8,"./controller/applicationController":9,"./controller/mainController":10,"./controller/newsController":11,"./controller/pageController":12,"./controller/splashController":13,"./metaService":15,"./routerConfig":16,"./utils/filter":17,"./utils/helper":18}],15:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -979,41 +979,7 @@ exports.default = ['$rootScope', '$http', '$timeout', function ($rootScope, $htt
 	});
 }];
 
-},{"./utils/helper":19}],16:[function(require,module,exports){
-"use strict";
-
-console.log("This is golden river!!!");
-//
-// //Analytics code...
-// //Google =>
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-//
-// ga('create', 'UA-77901914-1', 'auto');
-// ga('send', 'pageview');
-//
-// //Facebook essential =>
-// (function(d, s, id) {
-// 	var js, fjs = d.getElementsByTagName(s)[0];
-// 	if (d.getElementById(id)) return;
-// 	js = d.createElement(s); js.id = id;
-// 	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=108597779162841";
-// 	fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-//
-// //Facebook pixel =>
-// !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-// 	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-// 	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-// 	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-// 	document,'script','https://connect.facebook.net/en_US/fbevents.js');
-//
-// fbq('init', '578115232338331');
-// fbq('track', "PageView");
-
-},{}],17:[function(require,module,exports){
+},{"./utils/helper":18}],16:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -1095,7 +1061,7 @@ var newsRoute = {
 
 exports.default = routerConfig;
 
-},{"./utils/helper":19}],18:[function(require,module,exports){
+},{"./utils/helper":18}],17:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -1115,7 +1081,7 @@ function niceDate() {
 	};
 }
 
-},{}],19:[function(require,module,exports){
+},{}],18:[function(require,module,exports){
 (function (global){
 'use strict';
 
@@ -1315,4 +1281,4 @@ String.prototype.width = function (font) {
 global.uuid = generateNumberUUID;
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[14,16]);
+},{}]},{},[14]);
