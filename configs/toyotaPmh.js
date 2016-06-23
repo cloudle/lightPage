3d18d2de9f7bc5b3c6b470b377ed6558a8348f90
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-06-23 11:35:23 +0700

    ToyotaPMH update

--- configs/toyotaPmh.js
+++ configs/toyotaPmh.js
@@ -1,30 +1,26 @@
 module.exports = {
-  domain: "toyota-phumyhung.vn",
-  siteName: "toyotaPmh",
-  siteFav: "images/logo-urv.png",
+  domain: "toyota-phumyhung.com.vn",
+  siteName: "toyotaPMH",
+  siteFav: "images/logo-toyota.png",
   serverPort: 7023,
   production: true,
   apiHost: "http://103.56.157.66/realestate",
-  googleAnalyticId: "UA-77901914-4",
+  // scriptPatch: "./app/patches/anphaland.js",
+  cssPatch: "./app/style/patches/toyota.scss",
+  googleAnalyticId: "UA-77901914-5",
   facebookAppId: "",
   facebookPixelId: "",
   antsSiteId: "",
   antsRegisterGoalId: "",
   antsConversionId: "",
-  ogTitle: "United Realtors Vietnam - HỆ THỐNG PHÂN PHỐI BẤT ĐỘNG SẢN SỬ DỤNG CÔNG NGHỆ VƯỢT TRỘI",
-  ogImage: "https://c4.staticflickr.com/8/7425/27299346851_b454219c5a_o.jpg",
-  ogDescription: `Công ty UNITED REALTORS Việt Nam tự hào ứng dụng công nghệ siêu việt, đọc hiểu nhu cầu mua nhà của khách hàng quan tâm các dự án căn hộ Hot nhất khu Nam Sài Gòn.
-		Mỗi ngày hệ thống tiếp nhận hàng trăm lượt đăng ký quan tâm mua căn hộ.
-		Anh chị em tư vấn viên có nhiều năm kinh nghiệm và am hiểu thị trường Quận 7 hãy liên hệ ngay với chúng tôi để được cài đặt ứng dụng & nhận khách để bán hàng.`,
   translation: {
-    hotline: "0932 047 383",
+    hotline: "0907 331 339 - 0938 579 186",
     vietnamese: {
+      registerTitleHead: `
+				<span>Gọi </span>`,
       registerTitleTail: `
-			<span class="ultra strong" ng-bind="configs.translation.hotline"></span>
-			<span> hoặc gửi thông tin để</span> 
-			<span class="strong">THAM GIA</span>
-			<span>làm</span> 
-			<span class="strong">CỘNG TÁC VIÊN</span>`
+				<br>
+				<span>  hoặc điền thông tin để được hỗ trợ</span>`
     }
   }
 };
