fc889e1550551604a48371fdffa2a8a957395e31
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-07-06 14:41:54 +0700

    update configs

--- configs/rivercity.js
+++ configs/rivercity.js
@@ -4,7 +4,7 @@ module.exports = {
 	siteFav: "images/favicon.ico",
 	serverPort: 7020,
 	production: true,
-	apiHost: "http://localhost:8079/realestate",
+	apiHost: "http://103.56.157.66/realestate",
 	googleAnalyticId: "UA-77901914-1",
 	googleConversionId: 921153228,
 	googleConversionIdrmk: 921153228,
@@ -14,10 +14,10 @@ module.exports = {
 	antsSiteId: "578664336",
 	antsRegisterGoalId: "578664668",
 	antsConversionId: "f5ea3496",
-	languages: [
-		{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
-		{lang: "english", id: 2, display: "한국의"}
-	],
+	// languages: [
+	// 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+	// 	{lang: "english", id: 2, display: "한국의"}
+	// ],
 	ogTitle: "RIVER CITY - TUYỆT TÁC BÊN SÔNG SÀI GÒN.",
 	ogImage: "http://farm8.staticflickr.com/7369/27023964906_63062c471b_b.jpg",
 	ogDescription: `River City là dự án khu căn hộ có quy mô lớn nhất Nam Sài Gòn với 3 mặt tiếp giáp sông, được phát triển bởi An Gia - Creed Group (Nhật Bản) - Phát Đạt với tổng vốn đầu tư lên đến 500 triệu USD. 
