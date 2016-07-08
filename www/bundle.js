b610883a5ac2bb892183a3d005911a559f545363
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-07-08 15:14:11 +0700

    update configs

--- www/bundle.js
+++ www/bundle.js
@@ -1036,7 +1036,36 @@ exports.default = ['$rootScope', '$http', '$timeout', function ($rootScope, $htt
 },{"./utils/helper":19}],16:[function(require,module,exports){
 "use strict";
 
-console.log("This is anphaland!!!");
+console.log("This is golden river!!!");
+//
+// //Analytics code...
+// //Google =>
+// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+// 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+// 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+//
+// ga('create', 'UA-77901914-1', 'auto');
+// ga('send', 'pageview');
+//
+// //Facebook essential =>
+// (function(d, s, id) {
+// 	var js, fjs = d.getElementsByTagName(s)[0];
+// 	if (d.getElementById(id)) return;
+// 	js = d.createElement(s); js.id = id;
+// 	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=108597779162841";
+// 	fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+//
+// //Facebook pixel =>
+// !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// 	n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+// 	n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+// 	t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+// 	document,'script','https://connect.facebook.net/en_US/fbevents.js');
+//
+// fbq('init', '578115232338331');
+// fbq('track', "PageView");
 
 },{}],17:[function(require,module,exports){
 'use strict';
