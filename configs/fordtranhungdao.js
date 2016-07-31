bbac96554c23b1380da6878a070c2e9c737d1eb1
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-07-31 10:58:14 +0700

    update configs

---
+++ configs/fordtranhungdao.js
@@ -0,0 +1,41 @@
+module.exports = {
+    domain: "giadinhford.com.vn",
+    siteName: "fordphoquang",
+    siteFav: "images/vavicon.png",
+    serverPort: 7030,
+    production: true,
+    apiHost: "http://103.56.157.66/realestate",
+    cssPatch: "./app/style/patches/fordphoquang.scss",
+    googleAnalyticId: "UA-81118142-4",
+    facebookAppId: "",
+    googleConversionId:"877377975",
+    facebookPixelId: "",
+    antsSiteId: "579727739",
+    antsRegisterGoalId1: "579750460",
+    antsRegisterGoalId2: "579750461",
+    antsRegisterGoalId3: "579750462",
+    antsConversionId: "",
+    translation: {
+        hotline: "093 898 0139",
+        vietnamese: {
+            productnews: `PHIÊN BẢN KHÁC`,
+            childproduct: 'forddd',
+            news: 'TIN TỨC',
+            product: 'sản phẩm',
+            cardTitleHead: `<div style="font-size: 15px;">
+    ĐẠI LÝ SÀI GÒN FORD PHỔ QUANG 
+                            </div><br>
+                 <span style="font-size: 14px;">Hotline: </span>
+                               `,
+            cardTitleTail: `
+               <br><div style="font-size: 14px;margin: 10px 0;">Email: saigonford@saigonford.com.vn</div><div style="font-size: 14px;font-weight: 700;"></div>
+               <div style="font-size: 14px;margin: 10px 0;">Địa chỉ: 104 Phổ Quang, Phường 2, Quận Tân Bình, Hồ Chí Minh</div><div style="font-size: 14px;font-weight: 700;"></div>
+                                `,
+            registerTitleHead: `
+				<span>Liên hệ chúng tôi</span>`,
+            registerTitleTail: `
+				<br>
+				<span> để được hổ trợ</span>`
+        }
+    }
+};
