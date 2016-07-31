0bf685597ede1b822f7242abec2d97a59e985cbd
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-07-31 17:17:49 +0700

    update configs

---
+++ configs/fordcaothang.js
@@ -0,0 +1,41 @@
+module.exports = {
+    domain: "banxeonline.com.vn",
+    siteName: "Ford Cao Thắng",
+    siteFav: "images/vavicon.png",
+    serverPort: 7031,
+    production: true,
+    apiHost: "http://103.56.157.66/realestate",
+    cssPatch: "./app/style/patches/fordphoquang.scss",
+    googleAnalyticId: "UA-81118142-2",
+    facebookAppId: "",
+    googleConversionId:"877377975",
+    facebookPixelId: "",
+    antsSiteId: "579727748",
+    antsRegisterGoalId1: "579751744",
+    antsRegisterGoalId2: "579751745",
+    antsRegisterGoalId3: "579751746",
+    antsConversionId: "",
+    translation: {
+        hotline: "093 878 3139",
+        vietnamese: {
+            productnews: `PHIÊN BẢN KHÁC`,
+            home: 'TRANG CHỦ',
+            news: 'TIN TỨC',
+            product: 'sản phẩm',
+            cardTitleHead: `<div style="font-size: 15px;">
+    ĐẠI LÝ SÀI GÒN CAO THẮNG 
+                            </div><br>
+                 <span style="font-size: 14px;">Hotline: </span>
+                               `,
+            cardTitleTail: `
+               <br><div style="font-size: 14px;margin: 10px 0;">Email: saigonford@saigonford.com.vn</div><div style="font-size: 14px;font-weight: 700;"></div>
+               <div style="font-size: 14px;margin: 10px 0;">Địa chỉ: 61A Cao Thắng, Q.3, HCM</div><div style="font-size: 14px;font-weight: 700;"></div>
+                                `,
+            registerTitleHead: `
+				<span>Liên hệ chúng tôi</span>`,
+            registerTitleTail: `
+				<br>
+				<span> để được hổ trợ</span>`
+        }
+    }
+};
