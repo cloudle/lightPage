f30fe2f59cf9e3eddf2efb67f0b91922ab3b242e
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-08-01 14:34:31 +0700

    update new web millenium

---
+++ configs/millenium.js
@@ -0,0 +1,25 @@
+module.exports = {
+    domain: "millenium.org",
+    siteName: "Căn hộ Millenium",
+    siteFav: "images/logo-goldenriver.jpg",
+    serverPort: 7032,
+    production: false,
+    apiHost: "http://103.56.157.66/realestate",
+  //  scriptPatch: "./app/patches/goldenRiver.js",
+    cssPatch: "./app/style/patches/millenium.scss",
+    googleAnalyticId: "",
+    googleConversionId: "",
+    googleConversionLabel: "",
+    facebookAppId: "",
+    facebookPixelId: "",
+    antsSiteId: "",
+    antsRegisterGoalId: "",
+    antsConversionId: "",
+    ogTitle: "VINHOMES GOLDEN RIVER - ĐỈNH CAO CỦA CUỘC SỐNG THƯỢNG LƯU",
+    ogImage: "https://farm8.staticflickr.com/7163/26581800354_b82752ef56_o.jpg",
+    ogDescription: `Lần đầu tiên tại Việt Nam, bên dòng sông hiền hòa, giữa phố phường sôi động. Xuất hiện một quần thể đô thị sinh thái nằm trong lòng quận nhất. VINHOMES GOLDEN RIVER tọa lạc tại BA SON - quần thể đô thị sang trọng và hiện đại bật nhất Việt Nam. Mảnh đất kim cương giữa trung tâm thành phố. Trải nghiệm cuộc sống đẳng cấp, tận hưởng những tiện ích hoàn hảo.
+		Gọi 0902 623 673 (24/7) để nhận THÔNG TIN CHÍNH THỨC từ CHỦ ĐẦU TƯ`,
+    translation: {
+        hotline: "0902 623 673"
+    }
+};
