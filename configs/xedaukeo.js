98d459853113060dad4ed836adc5ccdb60dc6a0f
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-09-08 16:41:12 +0700

    update new tracking code

--- configs/xedaukeo.js
+++ configs/xedaukeo.js
@@ -1,29 +1,29 @@
 module.exports = {
-	domain: "xedaukeonhapkhau.vn",
-	siteName: "Xedaukeo",
-	siteFav: "images/xedaukeo_favicon.png",
-	serverPort: 7025,
-	production: true,
-	apiHost: "http://103.56.157.66/realestate",
-	// scriptPatch: "./app/patches/anphaland.js",
-	cssPatch: "./app/style/patches/xedaukeo.scss",
+  domain: "xedaukeonhapkhau.vn",
+  siteName: "Xedaukeo",
+  siteFav: "images/xedaukeo_favicon.png",
+  serverPort: 7025,
+  production: true,
+  apiHost: "http://103.56.157.66/realestate",
+  // scriptPatch: "./app/patches/anphaland.js",
+  cssPatch: "./app/style/patches/xedaukeo.scss",
   trackingnew: "abc",
-	googleAnalyticId: "UA-77901914-5",
-	facebookAppId: "",
-	facebookPixelId: "578115232338331",
-	antsSiteId: "579563692",
-	antsRegisterGoalId: "579563717",
-	antsConversionId: "",
-	translation: {
-		hotline: "0938 711 781",
-		vietnamese: {
+  googleAnalyticId: "UA-77901914-5",
+  facebookAppId: "",
+  facebookPixelId: "578115232338331",
+  antsSiteId: "579563692",
+  antsRegisterGoalId: "579563717",
+  antsConversionId: "",
+  translation: {
+    hotline: "0938 711 781",
+    vietnamese: {
       news: 'TIN TỨC',
       product: 'sản phẩm',
-			registerTitleHead: `
+      registerTitleHead: `
 				<span>Liên hệ chúng tôi</span>`,
-			registerTitleTail: `
+      registerTitleTail: `
 				<br>
 				<span> để được hổ trợ</span>`
-		}
-	}
+    }
+  }
 };
