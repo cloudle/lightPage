b2e2eeef7f2cc6f3230280abd43287ffbb02fbac
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-09-30 15:10:36 +0700

    update single page content for anphaland

--- app/controller/pageController.js
+++ app/controller/pageController.js
@@ -13,18 +13,19 @@ export class pageController {
 			previousGroup = $rootScope.activeGroup; $rootScope.activeGroup = parentGroup;
 
 		if(pageAlias == 'trang-chu') { $state.go('home'); return; }
-
 		//Kick back to the Home page if it's not a link in menu
 		if (!parentGroup || !parentGroup.children) {
 			$state.go('home');
 		} else if (parentGroup === previousGroup) { //If the parent group is already active => scroll to the child section!
+			let activeContentIndex = _.findIndex($rootScope.activeContents, {alias: pageAlias});
+			$rootScope.singleContent = $rootScope.activeContents[activeContentIndex];
 			//Scroll after 1 second to have better performance (after stuffs had been rendered).
-			$timeout(() => {
-				let scrollOffset = angular.element(`#section${pageAlias}`).offset().top - 50;
-				TweenLite.to(window, 1, {scrollTo:{y: scrollOffset}, ease:Power2.easeOut});
-			}, 800);
+			// $timeout(() => {
+			// 	let scrollOffset = angular.element(`#section${pageAlias}`).offset().top - 50;
+			// 	TweenLite.to(window, 1, {scrollTo:{y: scrollOffset}, ease:Power2.easeOut});
+			// }, 800);
+
 		} else { //Finally, load the page => set page's children content!
-			let loadedCount = 0; $rootScope.activeContents = [];
 			$window.scrollTo(0, 0); //Reset the scroll if loading from the beginning!
 			parentGroup.children.forEach((child, index) => {
 				$rootScope.activeContents[index] = {};
@@ -33,19 +34,31 @@ export class pageController {
 					if (childResult && childResult.Page) {
 						$rootScope.activeContents[index] = childResult.Page;
 					}
-				}).finally(() => {
-					loadedCount++;
-
-					if (loadedCount >= parentGroup.children.length) {
-						//Scroll after 1 second (after all content are ready from server!)
-						// to have better performance (after stuffs had been rendered).
-						$timeout(() => {
-							let scrollOffset = angular.element(`#section${pageAlias}`).offset().top - 50;
-							TweenLite.to(window, 1, {scrollTo:{y: scrollOffset}, ease:Power2.easeOut});
-						}, 500);
-					}
-				});
+				})
 			});
+			// parentGroup.children.forEach((child, index) => {
+			// 	$rootScope.activeContents[index] = {};
+			// 	$http.get(`${apiHost}/page/get/json`, { params: { domain, alias: child.alias } }).success(data => {
+			// 		let childResult = data.results[0];
+			// 		if (childResult && childResult.Page) {
+			// 			$rootScope.activeContents[index] = childResult.Page;
+			// 			if (pageAlias == childResult.Page.alias) {
+			// 				$rootScope.singleContent = childResult.Page;
+			// 			}
+			// 		}
+			// 	}).finally(() => {
+			// 		loadedCount++;
+      //
+			// 		// if (loadedCount >= parentGroup.children.length) {
+			// 		// 	//Scroll after 1 second (after all content are ready from server!)
+			// 		// 	// to have better performance (after stuffs had been rendered).
+			// 		// 	$timeout(() => {
+			// 		// 		let scrollOffset = angular.element(`#section${pageAlias}`).offset().top - 50;
+			// 		// 		TweenLite.to(window, 1, {scrollTo:{y: scrollOffset}, ease:Power2.easeOut});
+			// 		// 	}, 500);
+			// 		// }
+			// 	});
+			// });
 		}
 	}
 
