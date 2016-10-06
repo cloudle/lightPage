b65e70e69c588587cef42c5fba583578cef3463d
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-10-06 10:15:32 +0700

    Format for new site hungphatsilverstar

---
+++ configs/hungphatsilverstar.js
@@ -0,0 +1,28 @@
+module.exports = {
+  domain: "hungphatsilverstar.com",
+  siteName: "Hưng Phát Silver Star",
+  siteFav: "images/vaviconhungphat.png",
+  serverPort: 7034,
+  production: false,
+  cssPatch: "./app/style/patches/hungphatsilverstar.scss",
+  // marquee: {
+  // 	etext0: "Đặc biệt mở bán ",
+  // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
+  // 	etext2: "cho vị trí 1, 2, 23 được ",
+  // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
+  // 	img: "images/1-luong-vang.gif"
+  // },
+  apiHost: "http://103.56.157.66/realestate",
+  // languages: [
+  // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+  // 	{lang: "english", id: 2, display: "한국의"}
+  // ],
+  ogTitle: "RIVER CITY - TUYỆT TÁC BÊN SÔNG SÀI GÒN.",
+  ogImage: "http://farm8.staticflickr.com/7369/27023964906_63062c471b_b.jpg",
+  ogDescription: `River City là dự án khu căn hộ có quy mô lớn nhất Nam Sài Gòn với 3 mặt tiếp giáp sông, được phát triển bởi An Gia - Creed Group (Nhật Bản) - Phát Đạt với tổng vốn đầu tư lên đến 500 triệu USD. 
+		Với 4 tiện ích ưu việt: QUẢNG TRƯỜNG NƯỚC VÀ ÁNH SÁNG - BIỂN ĐẢO NHÂN TẠO DUBAI - HỒ BƠI THÁC NƯỚC 2 TẦNG - ĐƯỜNG HOA ĐI BỘ TRÊN KHÔNG
+    Gọi 0932 047 313 (24/7) để nhận  THÔNG TIN CHÍNH THỨC từ CHỦ ĐẦU TƯ`,
+  translation: {
+    hotline: "0905 935 935"
+  }
+};
