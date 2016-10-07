725608651c0730642719f54a899a12dd5e1fe3d0
nlmvy <nlmvy221@cusc.ctu.edu.vn>
2016-10-07 14:01:58 +0700

    Format for new site hungphatsilverstar

--- configs/hungphatsilverstar.js
+++ configs/hungphatsilverstar.js
@@ -17,12 +17,10 @@ module.exports = {
   // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
   // 	{lang: "english", id: 2, display: "한국의"}
   // ],
-  ogTitle: "RIVER CITY - TUYỆT TÁC BÊN SÔNG SÀI GÒN.",
-  ogImage: "http://farm8.staticflickr.com/7369/27023964906_63062c471b_b.jpg",
-  ogDescription: `River City là dự án khu căn hộ có quy mô lớn nhất Nam Sài Gòn với 3 mặt tiếp giáp sông, được phát triển bởi An Gia - Creed Group (Nhật Bản) - Phát Đạt với tổng vốn đầu tư lên đến 500 triệu USD. 
-		Với 4 tiện ích ưu việt: QUẢNG TRƯỜNG NƯỚC VÀ ÁNH SÁNG - BIỂN ĐẢO NHÂN TẠO DUBAI - HỒ BƠI THÁC NƯỚC 2 TẦNG - ĐƯỜNG HOA ĐI BỘ TRÊN KHÔNG
-    Gọi 0932 047 313 (24/7) để nhận  THÔNG TIN CHÍNH THỨC từ CHỦ ĐẦU TƯ`,
   translation: {
-    hotline: "0905 935 935"
+    vietnamese:{
+      registerTitleHead: `<span>Gửi thông tin để nhận</span>`,
+      registerTitleTail: `<span>BÁO GIÁ CHÍNH THỨC</span>`
+    }
   }
 };
