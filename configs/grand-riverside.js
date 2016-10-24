cdc4450894f52db658f0d272b3c505f04f763d85
vynlm <vynlm@twin.vn>
2016-10-24 11:31:33 +0700

    new site grandriverside

--- configs/grand-riverside.js
+++ configs/grand-riverside.js
@@ -1,12 +1,12 @@
 module.exports = {
     domain: "grand-riverside.vn",
     siteName: "Grand Riverside",
-    siteFav: "images/favicon.ico",
+    siteFav: "images/logo-navicon.png",
     serverPort: 7035,
     production: false,
     // marquee: {
     // 	etext0: "Đặc biệt mở bán ",
-    // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
+    // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",v
     // 	etext2: "cho vị trí 1, 2, 23 được ",
     // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
     // 	img: "images/1-luong-vang.gif"
