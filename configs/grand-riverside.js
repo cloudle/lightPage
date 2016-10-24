a95df5cb8e6f809d592db84bb2edbfd759359e8d
vynlm <vynlm@twin.vn>
2016-10-24 11:13:14 +0700

    ADD new code tracking

---
+++ configs/grand-riverside.js
@@ -0,0 +1,26 @@
+module.exports = {
+    domain: "grand-riverside.vn",
+    siteName: "Grand Riverside",
+    siteFav: "images/favicon.ico",
+    serverPort: 7035,
+    production: false,
+    // marquee: {
+    // 	etext0: "Đặc biệt mở bán ",
+    // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
+    // 	etext2: "cho vị trí 1, 2, 23 được ",
+    // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
+    // 	img: "images/1-luong-vang.gif"
+    // },
+    // apiHost: "http://103.56.157.66/realestate",
+    apiHost: "http://cms.twin.vn/",
+    // languages: [
+    // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+    // 	{lang: "english", id: 2, display: "한국의"}
+    // ],
+    ogTitle: "",
+    ogImage: "",
+    ogDescription: ``,
+    translation: {
+        hotline: "XXXX XXX XXX"
+    }
+};
