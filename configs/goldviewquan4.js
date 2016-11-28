862c3d1f1bec349532e7abec35d07b15f4c3e4d6
vynlm <vynlm@twin.vn>
2016-11-28 13:45:37 +0700

    clone new site goldviewquan4

---
+++ configs/goldviewquan4.js
@@ -0,0 +1,32 @@
+module.exports = {
+    domain: "goldviewquan4.com",
+    siteName: "Gold View Quan 4",
+    siteFav: "images/vaviconhungphat.png",
+    serverPort: 7036,
+    production: false,
+    cssPatch: "./app/style/patches/goldviewquan4.scss",
+    facebookPixelId: "",
+    // googleConversionHungPhatSilverStar: "871639554",
+    // antsInsightHungPhatSilverStar: "580521513",
+    // antsRegisterGoalId: "580521516",
+    // marquee: {
+    // 	etext0: "Đặc biệt mở bán ",
+    // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
+    // 	etext2: "cho vị trí 1, 2, 23 được ",
+    // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
+    // 	img: "images/1-luong-vang.gif"
+    // },
+    // apiHost: "http://103.56.157.66/realestate",K
+    // apiHost: "http://cms.twin.vn/",
+    apiHost: "http://cmsbackend.twin.vn/",
+    // languages: [
+    // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+    // 	{lang: "english", id: 2, display: "한국의"}
+    // ],
+    translation: {
+        vietnamese:{
+            registerTitleHead: `<span>Gửi thông tin để nhận</span>`,
+            registerTitleTail: `<br><span style="color: #FA8322;">BÁO GIÁ CHÍNH THỨC</span>`
+        }
+    }
+};
