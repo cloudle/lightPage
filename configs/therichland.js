2df8be6052efac9ee3d3a32baf520863bfbebba3
vynlm <vynlm@twin.vn>
2017-02-28 15:39:04 +0700

    new site the rich land

---
+++ configs/therichland.js
@@ -0,0 +1,29 @@
+module.exports = {
+    domain: "therichland.net",
+    siteName: "RICHland",
+    siteFav: "images/faveRichland.png",
+    serverPort: 7038,
+    production: false,
+    // iframeCodeNew: "run",
+    // googleAnalyticId: "UA-82993823-28",
+    // marquee: {
+    // 	etext0: "Đặc biệt mở bán ",
+    // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",v
+    // 	etext2: "cho vị trí 1, 2, 23 được ",
+    // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
+    // 	img: "images/1-luong-vang.gif"
+    // },
+    // apiHost: "http://103.56.157.66/realestate",
+    // apiHost: "http://cms.twin.vn/",
+    apiHost: "http://cmsbackend.twin.vn/",
+    // languages: [
+    // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+    // 	{lang: "english", id: 2, display: "한국의"}
+    // ],
+    ogTitle: "",
+    ogImage: "",
+    ogDescription: ``,
+    translation: {
+        hotline: "0977 XXX XXX"
+    }
+};
