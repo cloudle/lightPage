50f2c862ff5cb31d0815ab66efdb46a2e187eda8
vynlm <vynlm@twin.vn>
2017-08-11 17:08:50 +0700

    configs Google Analytics

--- configs/hungphatsilverstar.js
+++ configs/hungphatsilverstar.js
@@ -1,32 +1,33 @@
 module.exports = {
-  domain: "hungphatsilverstar.net.vn",
-  siteName: "Hưng Phát Silver Star",
-  siteFav: "images/vaviconhungphat.png",
-  serverPort: 7034,
-  production: true,
-  cssPatch: "./app/style/patches/hungphatsilverstar.scss",
-  facebookPixelId: "1762625813992970",
-  googleConversionHungPhatSilverStar: "871639554",
-  antsInsightHungPhatSilverStar: "580521513",
-  antsRegisterGoalId: "580521516",
-  // marquee: {
-  // 	etext0: "Đặc biệt mở bán ",
-  // 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
-  // 	etext2: "cho vị trí 1, 2, 23 được ",
-  // 	etext3: "TẶNG 2 LƯỢNG VÀNG",
-  // 	img: "images/1-luong-vang.gif"
-  // },
-  // apiHost: "http://103.56.157.66/realestate",
-  // apiHost: "http://cms.twin.vn/",
-  apiHost: "http://cmsbackend.twin.vn/",
-  // languages: [
-  // 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
-  // 	{lang: "english", id: 2, display: "한국의"}
-  // ],
-  translation: {
-    vietnamese:{
-      registerTitleHead: `<span>Gửi thông tin để nhận</span>`,
-      registerTitleTail: `<br><span style="color: #FA8322;">BÁO GIÁ CHÍNH THỨC</span>`
-    }
-  }
+	domain: "hungphatsilverstar.net.vn",
+	siteName: "Hưng Phát Silver Star",
+	siteFav: "images/vaviconhungphat.png",
+	serverPort: 7034,
+	production: true,
+	cssPatch: "./app/style/patches/hungphatsilverstar.scss",
+	facebookPixelId: "1762625813992970",
+	googleConversionHungPhatSilverStar: "871639554",
+	antsInsightHungPhatSilverStar: "580521513",
+	antsRegisterGoalId: "580521516",
+	googleAnalyticId: "UA-35891435-13",
+	// marquee: {
+	// 	etext0: "Đặc biệt mở bán ",
+	// 	etext1: "1 NGÀY DUY NHẤT 23/7 ",
+	// 	etext2: "cho vị trí 1, 2, 23 được ",
+	// 	etext3: "TẶNG 2 LƯỢNG VÀNG",
+	// 	img: "images/1-luong-vang.gif"
+	// },
+	// apiHost: "http://103.56.157.66/realestate",
+	// apiHost: "http://cms.twin.vn/",
+	apiHost: "http://cmsbackend.twin.vn/",
+	// languages: [
+	// 	{lang: "vietnamese", id: 1, display: "Tiếng Việt"},
+	// 	{lang: "english", id: 2, display: "한국의"}
+	// ],
+	translation: {
+		vietnamese:{
+			registerTitleHead: `<span>Gửi thông tin để nhận</span>`,
+			registerTitleTail: `<br><span style="color: #FA8322;">BÁO GIÁ CHÍNH THỨC</span>`
+		}
+	}
 };
